dbd287cef9f300b01c05ca319f850c71760406bc
DEV1 <DEV1@gmail.com>
2020-06-30 15:47:05 +0530

    Print message added

--- SampleProject/src/com/nt/test1/WishMessageGenarator.java
+++ SampleProject/src/com/nt/test1/WishMessageGenarator.java
@@ -4,8 +4,9 @@ public class WishMessageGenarator {
 	public String GetWishMessage(String user){
 		return "Hello"+user;
 	}
-
+public static void main(String[] args) {
 	WishMessageGenarator msg=new WishMessageGenarator();
-    
+    System.out.println(msg.GetWishMessage("raja"));
+   }    
 
 }
