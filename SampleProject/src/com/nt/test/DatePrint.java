3a3b8ba19d8d1e51168bc91d14974ad0b62d6ce3
DEV1 <DEV1@gmail.com>
2020-07-01 07:35:05 +0530

    sop added

--- SampleProject/src/com/nt/test/DatePrint.java
+++ SampleProject/src/com/nt/test/DatePrint.java
@@ -3,9 +3,11 @@ package com.nt.test;
 import java.util.Date;
 
 public class DatePrint {
-		
-		public Date printDate() {
+	  public Date printDate() {
 			return new Date();
 		}
-	
-}
+		public static void main(String[] args) {
+			
+    System.out.println("abc123");
+		}
+	}
