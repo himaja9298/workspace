3846fcbbefa77c13c5f9ef54ae464c8541d86f79
DEV1 <DEV1@gmail.com>
2020-07-01 11:40:44 +0530

    m4() added

--- SampleProject/src/com/nt/test/DatePrint.java
+++ SampleProject/src/com/nt/test/DatePrint.java
@@ -11,8 +11,12 @@ public class DatePrint {
 			System.out.println("Printdate m1 method");
 		}
      public void m2() {
-			System.out.println("Printdate m1 method");
+			System.out.println("Printdate m2 method");
 		}
+     public void m4() {
+			System.out.println("Printdate m4 method");
+		}
+
 
      public static void main(String[] args) {
 	 System.out.println("Hello");
